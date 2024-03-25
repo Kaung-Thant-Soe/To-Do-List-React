590310d3f05af39a5c97bbdad4197f21e2d26802
Kaung Thant Soe <lay54875@gmail.com>
2024-03-25 11:24:42 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const initialData = [
+  { id: 0, title: "Morning-Exercise", done: true },
+  { id: 1, title: "Breakfast", done: false },
+  { id: 2, title: "Study", done: false },
+];
+let nextId = 0;
+let nextDone;
+export default function App() {
+  nextId = initialData.id;
+  const [name, setName] = useState("");
+  const [datas, setData] = useState(initialData);
+  function handleAddTodo() {
+    setData([...datas, { id: nextId++, title: name }]);
+    setName("");
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <h1>To Do List</h1>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="inputValue"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {datas.map((item) => (
+          <li key={item.id}>
+            <input
+              className="box"
+              type="checkbox"
+              onChange={() => {
+                setData(datas.filter((a) => a.id !== item.id));
+              }}
+              checked={nextDone}
+            />
+
+            {item.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
